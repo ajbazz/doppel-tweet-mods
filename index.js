bf1c410ed0fa02197cf174b0c3c42fba80a213f2
Matthew Sells <mattms@tuta.io>
2017-09-03 12:21:24 -0500

    Adding library

--- index.js
+++ index.js
@@ -4,4 +4,4 @@
 */
 
 'use strict';
-module.exports = require('./lib/Doppel');
+module.exports = require('./lib/doppel');
--- test/test.js
+++ test/test.js
@@ -1,1 +1,5 @@
 var Doppel = require('../index.js');
+
+var config = {name: 'Ganglebot'};
+
+var d1 = new Doppel(config);
