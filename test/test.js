bf7847d0df57646a1fdd6e35e6b1dee80aaec998
Matthew Sells <mattms@tuta.io>
2017-09-06 17:49:08 -0500

    Getting stuff to work

--- test/test.js
+++ test/test.js
@@ -1,13 +1,18 @@
+// Import Doppel class
 var Doppel = require('../index.js');
+
+// Config for connecting to Twitter (usually use env variable)
 var twitterConfig = require('./config.js');
 
+// Set up configuration of Doppel
 var config = {
 	name: 'Ganglebot',
 	source: ['Ganglegasm'],
 	keys: twitterConfig
 };
 
+// Create new Doppel with configuration
 var d1 = new Doppel(config);
 
+// Testing
 d1.sayName();
-d1.getTweets();
