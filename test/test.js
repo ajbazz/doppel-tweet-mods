4c8cc90a42d3da91274f0bf238d7e870ecfc6323
Matthew Sells <mattms@tuta.io>
2017-09-25 18:56:54 -0500

    Updated user functions

--- test/test.js
+++ test/test.js
@@ -1,10 +1,5 @@
-// Import Doppel class
 var Doppel = require('../index.js');
-
-// Config for connecting to Twitter (usually use env variable)
 var twitterConfig = require('./config.js');
-
-var testText = '"You are the opposite of a Swiss Army knife, but, like a baby that looks worse."';
 
 function scrub(text){
 	return text.replace(/["]+/g, '');
@@ -13,9 +8,7 @@ function scrub(text){
 // Create new Doppel with configuration
 var d1 = new Doppel('timnocontext', twitterConfig, scrub);
 
-// Testing
-
 d1.update(function(){
 	var phrase = d1.generate();
-	// d1.tweet(phrase);
+	d1.tweet();
 });
